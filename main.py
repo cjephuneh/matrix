8438583d6c9567d69c2638f8b106047afb25e2d4
cjephuneh <calebjephuneh@gmail.com>
2023-11-15 15:38:20 +0000

    updates

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pyautogui
 import time
 
-pyautogui.FAILSAFE = False
+# Ensuring the failsafe is on, so you can stop the script by moving the mouse to the upper left corner.
+pyautogui.FAILSAFE = True
 
 while True:
-    time.sleep(15)
-    for i in range(0, 100):
-        pyautogui.moveTo(0, i * 5)
-    for i in range(0, 3):
-        pyautogui.press('shift')
+    # Move the mouse cursor in a small pattern
+    pyautogui.move(100, 0, duration=1)  # Right
+    pyautogui.move(0, 100, duration=1)  # Down
+    pyautogui.move(-100, 0, duration=1)  # Left
+    pyautogui.move(0, -100, duration=1)  # Up
+
+    # Press the 'shift' key after moving the mouse
+    pyautogui.press('shift')
+
+    # Wait for 5 seconds before moving the mouse again
+    time.sleep(5)
